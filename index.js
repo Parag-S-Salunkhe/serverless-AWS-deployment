d8289e5acfb36c5837e2b7d1ed24baf1f739aef7
ParagS <paragsalunkhe27@gmail.com>
2023-06-17 12:11:41 +0200

    views counter

---
+++ index.js
@@ -0,0 +1,7 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter(){
+    let response = await fetch("https://z4b3a7w6lttjihzcr2ezkrlpkq0pmftx.lambda-url.eu-west-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = 'Views: ${data}';
+}
+updateCounter();
