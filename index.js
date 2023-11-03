7d459569f3c88ef6751e090a3bb6f755d8ece9b5
ParagS <paragsalunkhe27@gmail.com>
2023-11-03 14:30:30 +0000

    view counter

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter(){
     let response = await fetch("https://z4b3a7w6lttjihzcr2ezkrlpkq0pmftx.lambda-url.eu-west-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = 'Views: ${data}';
+    counter.innerHTML = 'Views' + data;
 }
 updateCounter();
